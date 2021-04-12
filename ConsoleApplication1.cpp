b5342aba2d88ae080086366f94f42d97ab13fb4e
Jozef Izso <jozef.izso@gmail.com>
2021-04-12 17:25:04 +0200

    Dump DPI_AWARENESS_CONTEXT values

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,11 +5,16 @@
 
 int main()
 {
+    DPI_AWARENESS_CONTEXT unaware = DPI_AWARENESS_CONTEXT_UNAWARE;
+    DPI_AWARENESS_CONTEXT system = DPI_AWARENESS_CONTEXT_SYSTEM_AWARE;
+    DPI_AWARENESS_CONTEXT perMonitor = DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE;
+    DPI_AWARENESS_CONTEXT perMonitorV2 = DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2;
+
 
     DPI_AWARENESS_CONTEXT ctx0 = GetThreadDpiAwarenessContext();
     DPI_AWARENESS a0 = GetAwarenessFromDpiAwarenessContext(ctx0);
 
-    DPI_AWARENESS_CONTEXT p1 = SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+    DPI_AWARENESS_CONTEXT p1 = SetThreadDpiAwarenessContext(perMonitorV2);
     DPI_AWARENESS_CONTEXT ctx1 = GetThreadDpiAwarenessContext();
     DPI_AWARENESS ap1 = GetAwarenessFromDpiAwarenessContext(p1);
     DPI_AWARENESS a1 = GetAwarenessFromDpiAwarenessContext(ctx1);
