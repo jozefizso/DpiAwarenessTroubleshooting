bd94737e0b9a7de9aa2251b710fef8c9ee83959e
Jozef Izso <jozef.izso@gmail.com>
2021-04-12 17:10:38 +0200

    Add project files.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+
+    DPI_AWARENESS_CONTEXT ctx0 = GetThreadDpiAwarenessContext();
+    DPI_AWARENESS a0 = GetAwarenessFromDpiAwarenessContext(ctx0);
+
+    DPI_AWARENESS_CONTEXT p1 = SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+    DPI_AWARENESS_CONTEXT ctx1 = GetThreadDpiAwarenessContext();
+    DPI_AWARENESS ap1 = GetAwarenessFromDpiAwarenessContext(p1);
+    DPI_AWARENESS a1 = GetAwarenessFromDpiAwarenessContext(ctx1);
+
+    DPI_AWARENESS_CONTEXT p2 = SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_SYSTEM_AWARE);
+    DPI_AWARENESS_CONTEXT ctx2 = GetThreadDpiAwarenessContext();
+    DPI_AWARENESS ap2 = GetAwarenessFromDpiAwarenessContext(p2);
+    DPI_AWARENESS a2 = GetAwarenessFromDpiAwarenessContext(ctx2);
+
+    DPI_AWARENESS_CONTEXT ctxX = SetThreadDpiAwarenessContext(p1);
+    DPI_AWARENESS aX = GetAwarenessFromDpiAwarenessContext(ctxX);
+
+    std::cout << "Hello World!\n";
+}
